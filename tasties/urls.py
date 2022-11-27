c0f8e7480145864a927ad925077861765445499e
Efraim Vishnevetsky <efraimvishnivetzky@gmail.com>
2022-11-27 15:43:39 +0200

    Adding PyTest workflow for running tests and modifying django application name
    - Added pytest.yml, a workflow that will run the pytest tests we provide for the Django code in our PRs
    - Changed main django app name to "tasties_app" instead of "webpage"

--- tasties/urls.py
+++ tasties/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webpage import views
+from tasties_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name="index")
+    path('', views.index, name="index")
 ]
