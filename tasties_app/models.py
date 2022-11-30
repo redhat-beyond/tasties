90d130aead4c8a65fbb65630043623a84874c6a5
GalOved <galoved94@gmail.com>
2022-11-30 15:23:56 +0200

    Adding recipe model
    Adding recipe model to tasties app.
    Add a edit methods to the recipe model.
    Adding a test for the recipe model and for the methods.

--- tasties_app/models.py
+++ tasties_app/models.py
@@ -1,3 +1,86 @@
-# from django.db import models
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MinLengthValidator, MaxValueValidator
 
-# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=16, unique=True)
+
+
+class Recipe(models.Model):
+    """
+
+    This class represent Recipe object
+
+    Fields:
+        title (string) : Title of the recipe.
+        author_id (User) : User object.
+        categories (Categories) : Categories object.
+        description (string) : Description of the recipe.
+        directions (string) : Directions how to make the recipe.
+        publication_date (DateTime) : When was the recipe published.
+        minutes_to_make (int) : How long does it take to make the recipe.
+        recipe_picture (Image) : A picture that describes the recipe.
+
+    """
+    title = models.CharField(max_length=64, validators=[MinLengthValidator(1)], unique=True)
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    description = models.CharField(max_length=512, validators=[MinLengthValidator(1)])
+    directions = models.CharField(max_length=65536, validators=[MinLengthValidator(1)])
+    publication_date = models.DateTimeField(auto_now_add=timezone.now())
+    minutes_to_make = models.IntegerField(validators=[MinValueValidator(1)])
+    recipe_picture = models.ImageField(upload_to='recipe_pictures')
+
+    def edit_recipe(self, new_title, new_description,
+                    new_directions, new_minutes_to_make,
+                    new_recipe_pic):
+        """
+        This function designed to allow the user to edit his recipe.
+
+        Fields:
+            new_title (string) : The new given title to the recipe.
+            new_description (string) : The new or edited recipe description
+                                        given to the recipe.
+            new_directions (string) : The new or edited directions given to the recipe.
+            new_minutes_to_make (int) : The updating the preparation time of the recipe.
+            new_recipe_pic (Image) : The new picture that describes the recipe.
+        """
+        self.title = new_title
+        self.description = new_description
+        self.directions = new_directions
+        self.minutes_to_make = new_minutes_to_make
+        self.recipe_picture = new_recipe_pic
+        self.save()
+
+
+class Ingredient(models.Model):
+
+    class UnitChoices(models.TextChoices):
+
+        WHOLE = "Whole"
+        FLOZ = "Fluid Ounce"
+        TSP = "Tea Spoon"
+        OZ = "Ounce"
+        CUP = "Cup"
+        GRAM = "Gram"
+        ML = "Milliliter"
+
+    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.0001)])
+    measurement_unit = models.CharField(max_length=11, choices=UnitChoices.choices, default=UnitChoices.WHOLE)
+    description = models.CharField(max_length=64, validators=[MinLengthValidator(1)])
+
+
+class Comment(models.Model):
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    publication_date = models.DateField(default=timezone.now)
+    comment_text = models.CharField(max_length=512, validators=[MinLengthValidator(1)])
+
+
+class Rating(models.Model):
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
---
+++ tasties_app/tests/test_recipe.py
@@ -0,0 +1,63 @@
+import pytest
+from tasties_app.models import Recipe, Category
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+@pytest.fixture
+def generate_data():
+    """This fixture create the data we need for tests
+    and save it in the data base. """
+
+    user1 = User.objects.create_user(username='john', password='password')
+
+    test_data = ['Test Recipe1', user1, 'Test Description1', 'Test Directions1', 1, 'test_picture1']
+
+    category1 = Category.objects.create(category_name="1")
+    recipe1 = Recipe(title=test_data[0], author_id=test_data[1], description=test_data[2], directions=test_data[3],
+                     publication_date=timezone.now(), minutes_to_make=test_data[4], recipe_picture=test_data[5])
+
+    category1.save()
+    recipe1.save()
+    recipe1.categories.add(category1)
+    recipe1.save()
+    category1.save()
+
+
+class TestRecipeModel:
+
+    @pytest.mark.django_db
+    def test_save_recipe(self, generate_data):
+        recipe_set = Recipe.objects.all()
+        assert len(recipe_set) == 1
+
+    @pytest.mark.django_db
+    def test_field_recipe(self, generate_data):
+        recipe_set = Recipe.objects.all()
+        recipe1 = recipe_set[0]
+        category1 = Category.objects.first()
+        assert recipe1.title == "Test Recipe1"
+        assert recipe1.author_id.username == "john"
+        assert category1.category_name == "1"
+        assert recipe1.description == "Test Description1"
+        assert recipe1.directions == "Test Directions1"
+        assert recipe1.minutes_to_make == 1
+        assert recipe1.recipe_picture == "test_picture1"
+
+    @pytest.mark.django_db
+    def test_edit_recipe(self, generate_data):
+        recipe_set = Recipe.objects.all()
+        recipe1 = recipe_set[0]
+        recipe1.edit_recipe("Test Recipe2", "Test Description2", "Test Directions2", 5, "test_picture2")
+        assert recipe1.title == "Test Recipe2"
+        assert recipe1.description == "Test Description2"
+        assert recipe1.directions == "Test Directions2"
+        assert recipe1.minutes_to_make == 5
+        assert recipe1.recipe_picture == "test_picture2"
+
+    @pytest.mark.django_db
+    def test_delete_recipe(self, generate_data):
+        recipe_set = Recipe.objects.all()
+        recipe1 = recipe_set[0]
+        recipe1.delete()
+        assert len(recipe_set) == 0
