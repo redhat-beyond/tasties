7f90865d0f3fc1aa91626bfb2a4e787ed29b139d
GalOved <galoved94@gmail.com>
2022-12-05 20:32:12 +0200

    Add models file with out conflicting

--- tasties_app/models.py
+++ tasties_app/models.py
@@ -1,7 +1,13 @@
+from enum import Enum
+
+from django.contrib.auth.models import User
+from django.core.validators import (
+    MaxValueValidator,
+    MinLengthValidator,
+    MinValueValidator,
+)
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator, MinLengthValidator, MaxValueValidator
 
 
 class Category(models.Model):
@@ -10,10 +16,6 @@ class Category(models.Model):
 
 class Recipe(models.Model):
     """
-<<<<<<< HEAD
-
-=======
->>>>>>> a51488b (Update Recipe model.)
     This class represent Recipe object
 
     Fields:
@@ -27,41 +29,16 @@ class Recipe(models.Model):
         recipe_picture (Image) : A picture that describes the recipe.
 
     """
-<<<<<<< HEAD
-    title = models.CharField(max_length=64, validators=[MinLengthValidator(1)], unique=True)
-=======
     title = models.CharField(
         max_length=64, validators=[MinLengthValidator(1)], unique=True
     )
->>>>>>> a51488b (Update Recipe model.)
     author_id = models.ForeignKey(User, on_delete=models.CASCADE)
     categories = models.ManyToManyField(Category)
     description = models.CharField(max_length=512, validators=[MinLengthValidator(1)])
     directions = models.CharField(max_length=65536, validators=[MinLengthValidator(1)])
-    publication_date = models.DateTimeField(auto_now_add=timezone.now())
+    publication_date = models.DateField(default=timezone.now)
     minutes_to_make = models.IntegerField(validators=[MinValueValidator(1)])
-    recipe_picture = models.ImageField(upload_to='recipe_pictures')
-
-    def edit_recipe(self, new_title, new_description,
-                    new_directions, new_minutes_to_make,
-                    new_recipe_pic):
-        """
-        This function designed to allow the user to edit his recipe.
-
-        Fields:
-            new_title (string) : The new given title to the recipe.
-            new_description (string) : The new or edited recipe description
-                                        given to the recipe.
-            new_directions (string) : The new or edited directions given to the recipe.
-            new_minutes_to_make (int) : The updating the preparation time of the recipe.
-            new_recipe_pic (Image) : The new picture that describes the recipe.
-        """
-        self.title = new_title
-        self.description = new_description
-        self.directions = new_directions
-        self.minutes_to_make = new_minutes_to_make
-        self.recipe_picture = new_recipe_pic
-        self.save()
+    recipe_picture = models.ImageField(upload_to="recipe_pictures")
 
     def edit_recipe(self, new_title, new_description,
                     new_directions, new_minutes_to_make,
@@ -106,9 +83,7 @@ class Recipe(models.Model):
 
 
 class Ingredient(models.Model):
-
-    class UnitChoices(models.TextChoices):
-
+    class UnitChoices(models.TextChoices, Enum):
         WHOLE = "Whole"
         FLOZ = "Fluid Ounce"
         TSP = "Tea Spoon"
@@ -118,8 +93,12 @@ class Ingredient(models.Model):
         ML = "Milliliter"
 
     recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.0001)])
-    measurement_unit = models.CharField(max_length=11, choices=UnitChoices.choices, default=UnitChoices.WHOLE)
+    amount = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(0.0001)]
+    )
+    measurement_unit = models.CharField(
+        max_length=11, choices=UnitChoices.choices, default=UnitChoices.WHOLE
+    )
     description = models.CharField(max_length=64, validators=[MinLengthValidator(1)])
 
 
@@ -131,6 +110,28 @@ class Comment(models.Model):
 
 
 class Rating(models.Model):
+    """
+    This Class represent Rating object
+
+    Fields:
+        author_id (User): User object.
+        recipe_id (Recipe): Recipe object.
+        rating (int): rating for the recipe, 1-5
+    """
+
     author_id = models.ForeignKey(User, on_delete=models.CASCADE)
     recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+
+    def update_rating(self, new_rate):
+        """
+        This function update the rating for given recipe
+
+        Args:
+            new_rate(int): the new rating for the recipe
+        """
+        if type(new_rate) != int or new_rate > 5 or new_rate < 1:
+            raise ValueError("Invalid value")
+        self.rating = new_rate
