0b2fdc2983f4914775eac52f90856c6608d01dd7
Gil Shechter <gilshechter13@gmail.com>
2022-12-28 23:29:16 +0200

    Adding base template
    Building the html file that contains the header and footer of the app
    Adding route for the template
    Adding view function

--- tasties_app/views.py
+++ tasties_app/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'tasties_app/index.html',)
-# Create your views here.
+
+
+def base(request):
+    return render(request, 'tasties_app/base.html',)
