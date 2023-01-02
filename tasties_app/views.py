44fbf4f1af8670c6cd70a9fa02b933fdf8b6a42d
GalOved <galoved94@gmail.com>
2023-01-02 23:08:27 +0200

    Adding a feature for categories
    Adding a feature of showing all the categories are found in the database
    and showing tham on the html file.

--- tasties_app/views.py
+++ tasties_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from tasties_app.models import Category
 
 
 def index(request):
@@ -6,4 +7,5 @@ def index(request):
 
 
 def base(request):
-    return render(request, 'tasties_app/base.html',)
+    objects = Category.objects.all()
+    return render(request, 'tasties_app/base.html', {'objects': objects})
