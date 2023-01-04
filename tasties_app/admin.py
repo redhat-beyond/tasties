9e91c581ed34447643e2fc1b20fb568931f3fa85
WaseemAbuSobe <waseem-abusobe@hotmail.com>
2023-01-04 21:12:24 +0200

    modified search bar and active search bar

--- tasties_app/admin.py
+++ tasties_app/admin.py
@@ -1,3 +1,10 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Comment, Rating, Recipe, Ingredient
+
+admin.site.register(Rating)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
--- tasties_app/views.py
+++ tasties_app/views.py
@@ -9,14 +9,29 @@ def index(request):
 
 
 def base(request):
-    return render(request, 'tasties_app/base.html',)
+    search_input = request.POST.get('search')
+    if search_input:
+        return recipes(request, search_input)
+    else:
+        return render(request, 'tasties_app/base.html')
 
 
-def recipes(request):
-    recipes_list = Recipe.objects.all()
-    recipes_with_ratings = {}
-    for recipe in recipes_list:
-        recipes_with_ratings[recipe] = Rating.objects.filter(recipe_id=recipe).aggregate(Avg('rating'))['rating__avg']
-    recipes_with_ratings = OrderedDict(sorted(recipes_with_ratings.items(), key=lambda x: x[1], reverse=True))
-    context = {'recipes_with_ratings': recipes_with_ratings}
-    return render(request, 'tasties_app/recipes.html', context)
+def recipes(request, search_value=None):
+    recipes_list = Recipe.objects.filter(title__icontains=search_value)
+    if recipes_list:
+        recipes_with_ratings = {}
+        for recipe in recipes_list:
+            recipes_with_ratings[recipe] = Rating.objects.filter(recipe_id=recipe).aggregate(Avg('rating'))
+            ['rating__avg']
+            recipes_with_ratings = OrderedDict(sorted(recipes_with_ratings.items(), key=lambda x: x[1], reverse=True))
+        context = {'recipes_with_ratings': recipes_with_ratings}
+        return render(request, 'tasties_app/recipes.html', context)
+    else:
+        recipes_list = Recipe.objects.all()
+        recipes_with_ratings = {}
+        for recipe in recipes_list:
+            recipes_with_ratings[recipe] = Rating.objects.filter(recipe_id=recipe).aggregate(Avg('rating'))
+            ['rating__avg']
+            recipes_with_ratings = OrderedDict(sorted(recipes_with_ratings.items(), key=lambda x: x[1], reverse=True))
+        context = {'recipes_with_ratings': recipes_with_ratings}
+        return render(request, 'tasties_app/recipes.html', context)
