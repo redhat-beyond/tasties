86e8d5b590898139084878f0924f35e729dc051a
Gil Shechter <gilshechter13@gmail.com>
2023-01-10 22:08:51 +0200

    Activate the view-recipe from recipes menu
    Each recipe in the recipes menu should be clickable and should
    redirect to its own page.
    Modified the tests to check the view-recipe page.

--- tasties/urls.py
+++ tasties/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('login/', views.login_user, name="login"),
     path('register/', views.register, name="register"),
     path('logout/', views.logout_user, name="logout"),
-    path('view_recipe/', views.view_recipe, name="view_recipe")
+    path('view_recipe/<int:recipe_id>/', views.view_recipe, name="view_recipe")
 ]
