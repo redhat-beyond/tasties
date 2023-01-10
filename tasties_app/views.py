553e79f75e2c38c8311390a61556cdd566c4f5aa
GalOved <galoved94@gmail.com>
2023-01-10 20:34:12 +0200

    Adding comment feathure for view recipe page
    Adding a feathure for view recipe page that show all the comments are users gave to the recipe,
    and a feathure that users can add a new comment for a recipe.

--- tasties_app/views.py
+++ tasties_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
-from tasties_app.models import Recipe, Rating
+from tasties_app.models import Recipe, Rating, Comment
 from django.db.models import Avg
 from collections import OrderedDict
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import CreateUserForm
 from django.contrib import messages
+from django.utils import timezone
 
 
 def base(request):
@@ -72,5 +73,19 @@ def view_recipe(request, recipe_id=None):
     ingredients = recipe.ingredient_set.all()
     rating = recipe.rating_set.aggregate(Avg('rating'))['rating__avg']
     categories = recipe.categories.all()
-    context = {'recipe': recipe, 'ingredients': ingredients, 'rating': rating, 'categories': categories}
+    if request.method == 'POST':
+        add_comment(request, recipe.title)
+    comments = Comment.objects.filter(recipe_id=recipe.id)
+    context = {'recipe': recipe, 'ingredients': ingredients, 'rating': rating,
+               'categories': categories, 'comments': comments}
     return render(request, 'tasties_app/view_recipe.html', context)
+
+
+@login_required(login_url='login')
+def add_comment(request, recipe_title):
+    comment_value = request.POST.get('comment-adding')
+    recipe = Recipe.objects.get(title=recipe_title)
+    user = request.user
+    comment_input = Comment(author_id=user, recipe_id=recipe, publication_date=timezone.now(),
+                            comment_text=comment_value)
+    comment_input.save()
